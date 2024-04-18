8daca385ea2d4e37488a86c422ad84455bc2e561
EaeDave <daveisrd@gmail.com>
2024-04-18 19:29:15 -0300

    Aula 3 - console.log - A primeira coisa que você vai ver em JS

---
+++ aula3/index.js
@@ -0,0 +1,7 @@
+// String = Texto
+console.log('"David Rodrigues"');  
+console.log("'David Rodrigues'");  
+console.log(`'David' "Rodrigues"`);
+
+// Number = Número
+console.log(22, "anos de idade");  
