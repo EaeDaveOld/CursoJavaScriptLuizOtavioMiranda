bea000fd461c54ea5f94979fe7cfb4ce5b5cb977
EaeDave <daveisrd@gmail.com>
2024-04-19 12:34:09 -0300

    Aula 4 - Comentários de código

---
+++ aula4/comentarios.js
@@ -0,0 +1,15 @@
+// Comentário simples (comentário de uma única linha)
+console.log("Olá, Mundo!")  // Aqui tem outro comentário
+console.log("Linha 1")
+// Comentário
+
+/*
+* Um bloco de código (comentário de multiplas linhas)
+* Test 1
+* Test 2
+* Test 3
+*/
+console.log("Linha 2")
+console.log("Linha 3")
+console.log("Linha 4")
+// Vai ser ignorado
