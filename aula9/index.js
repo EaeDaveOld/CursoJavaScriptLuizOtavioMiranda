9c93e8a97e6f66deea28962b35239059040b612d
EaeDave <daveisrd@gmail.com>
2024-04-19 15:30:30 -0300

    Aula 9 - Let vs Var - Primeira diferença

---
+++ aula9/index.js
@@ -0,0 +1,6 @@
+global = "Valor no escopo global";  // Declarar variáveis assim não é recomendado pois está no escopo global
+console.log(global);
+
+var nome = "David";
+var nome = "Rodrigues";  // É possível redeclarar variáveis usando var (não recomendado) | Usando let isso não é possível
+console.log(nome);
