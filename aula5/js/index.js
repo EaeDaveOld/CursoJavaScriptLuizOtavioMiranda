736d9837b2a0171748ebd73da0c8394887f6a693
EaeDave <daveisrd@gmail.com>
2024-04-19 12:54:32 -0300

    Navegador vs Node (HTML + JavaScript)

--- aula5/js/index.js
+++ aula5/js/index.js
@@ -1,4 +1,4 @@
 // Um comentário
 console.log('Olá, Mundo!');
 console.log('Estou sendo exibido no Console do navegador!');
-alert("Olá, Mundo!")
+alert("Olá, Mundo!")  // Este alert só será exibido no navegador, aqui dentro do Node acontecerá um erro.
