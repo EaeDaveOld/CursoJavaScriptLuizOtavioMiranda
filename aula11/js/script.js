cc94d399f504111da1aa9be379411ca9fbdc31c3
EaeDave <daveisrd@gmail.com>
2024-04-20 14:02:19 -0300

    Aula 11 - Exercícios - Solução

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -2,10 +2,15 @@ continuarPrograma = true;
 while (continuarPrograma) {
     alert('Soma de dois números - Pressione [OK]');
 
-    let primeiroNumero = parseInt(prompt("Digite o primeiro número que será somado: "));
+    let primeiroNumero = prompt("Digite o primeiro número que será somado: ");
+    primeiroNumero = Number(primeiroNumero);  // Convertendo de string para number
 
-    let segundoNumero = parseInt(prompt("Digite o segundo número que será somado: "));
-    alert(`A soma de ${primeiroNumero} + ${segundoNumero} = ${primeiroNumero + segundoNumero}`);
+    let segundoNumero = prompt("Digite o segundo número que será somado: ");
+    segundoNumero = Number(segundoNumero);  // Convertendo de string para number
+
+    let resultadoSoma = primeiroNumero + segundoNumero;
+
+    alert(`A soma de ${primeiroNumero} + ${segundoNumero} = ${resultadoSoma}`);
     let continuarPrograma = confirm("Deseja calcular novamente?");
 
     if (continuarPrograma == false) {
