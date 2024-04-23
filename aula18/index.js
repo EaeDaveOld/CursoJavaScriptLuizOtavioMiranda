b43096ab18269e3a612080e4b3658f4be9a0dfcd
EaeDave <daveisrd@gmail.com>
2024-04-23 15:00:25 -0300

    Aula 18 - Objetos (Básico)

---
+++ aula18/index.js
@@ -0,0 +1,40 @@
+const pessoa1 = {  // Declarando um objeto literal
+    nome: "David",
+    sobrenome: "Rodrigues",
+    idade: 22,
+
+    fala() {  // Isso é um método do objeto pessoa1
+        console.log(`A minha idade atual é ${this.idade}`);
+    },
+
+    incrementaIdade() {  // Este é outro método do objeto pessoa1, e o "this" referencia o próprio objeto.
+        ++this.idade;
+    },
+};
+
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+
+console.log(pessoa1.nome);
+console.log(pessoa1.sobrenome);
+console.log(pessoa1.idade);
+
+function criaPessoa(nome, sobrenome, idade) {  // Função Factory (Cria objetos)
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade,
+    };
+}
+
+const pessoa2 = criaPessoa("Camila", "Santos", 23);
+const pessoa3 = criaPessoa("Maria", "Oliveira", 40);
+const pessoa4 = criaPessoa("Luiz", "Viana", 30);
+const pessoa5 = criaPessoa("Goku", "Kakaroto", 28);
+console.log(pessoa2.nome);
+console.log(pessoa2.sobrenome);
+console.log(pessoa2.idade);
