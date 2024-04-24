fef2d688d578e0296d05315c0cc38e0f76dea534
EaeDave <daveisrd@gmail.com>
2024-04-24 15:13:39 -0300

    AUla 25 - if, else if e else(2)

---
+++ aula25/index.js
@@ -0,0 +1,15 @@
+const numero = 5;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5.');
+} else {
+    console.log('O número não está entre 0 e 5');
+}
+
+const num1 = 5;
+const num2 = 10;
+const quinze = 15;
+
+if (num1 + num2 === quinze) {
+    console.log(`A soma entre ${num1} + ${num2} = ${quinze}`);
+}
